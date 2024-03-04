45f35eb43ea69acddfb640164b1b390670fd2a40
Andreas <andreas.kruehlmann@siemens.com>
2024-03-04 18:00:32 +0100

    Mark test as quick

--- test/test.ml
+++ test/test.ml
@@ -1,3 +1,3 @@
 let () =
   let open Alcotest in
-  run "Daemon" [("run_daemon", [("test_run_daemon", `Slow, Test_daemon.run_test)])]
+  run "Daemon" [("run_daemon", [("test_run_daemon", `Quick, Test_daemon.run_test)])]
