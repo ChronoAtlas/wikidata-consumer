ae0db2081766b2cd28d1894e88f161ffc07769f3
Andreas Krühlmann <andreas@kruhlmann.dev>
2024-03-04 00:15:10 +0000

    Add nix.shell

---
+++ shell.nix
@@ -0,0 +1,29 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+
+pkgs.mkShell{
+  packages = with pkgs; [
+    libpqxx
+    ocamlPackages.alcotest
+    ocamlPackages.base
+    ocamlPackages.bisect_ppx
+    ocamlPackages.cmdliner
+    ocamlPackages.kafka_lwt
+    ocamlPackages.lwt
+    ocamlPackages.ocaml
+    ocamlPackages.ocamlformat
+    ocamlPackages.postgresql
+    ocamlPackages.utop
+    ocamlPackages.yojson
+    opam
+    pkg-config
+    rdkafka
+    zlib
+  ];
+  shellHook = ''
+    exec zsh
+  '';
+}
