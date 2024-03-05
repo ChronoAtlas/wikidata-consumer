82d2230be7b1eae9c65f0c6afac3e0f92296f292
Andreas <andreas.kruehlmann@siemens.com>
2024-03-05 16:00:32 +0100

    Fix match lint

--- bin/Wikidata_migrate.ml
+++ bin/Wikidata_migrate.ml
@@ -4,12 +4,12 @@ let () =
   let migration_method = Sys.argv.(1) in
   let conn_str = Sys.argv.(2) in
   match migration_method with
-    | "upgrade" -> begin match Migration.upgrade conn_str with
-        | Ok () -> Printf.printf "Upgrade succeeded\n"
-        | _ -> Printf.printf "Upgrade failed\n"
-    end
-    | "downgrade" -> begin match Migration.downgrade conn_str with
-        | Ok () -> Printf.printf "Downgrade succeeded\n"
-        | _ -> Printf.printf "Downgrade failed\n"
-    end
-    | _ -> Printf.printf "Invalid method %s\n" migration_method
+  | "upgrade" -> (
+    match Migration.upgrade conn_str with
+    | Ok () -> Printf.printf "Upgrade succeeded\n"
+    | _ -> Printf.printf "Upgrade failed\n" )
+  | "downgrade" -> (
+    match Migration.downgrade conn_str with
+    | Ok () -> Printf.printf "Downgrade succeeded\n"
+    | _ -> Printf.printf "Downgrade failed\n" )
+  | _ -> Printf.printf "Invalid method %s\n" migration_method
